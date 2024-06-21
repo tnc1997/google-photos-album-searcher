8bd35d51a11c353ce300dcb6f2ef08cd397ab839
Thomas Clark <tnc1997@outlook.com>
2024-06-21 21:21:39 +0100

    chore: update terms of service text

--- lib/authentication/sign_in_card.dart
+++ lib/authentication/sign_in_card.dart
@@ -1,6 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
-import '../common/terms_of_service_text.dart';
 import 'sign_in_button.dart';
 
 class SignInCard extends StatelessWidget {
@@ -28,13 +29,91 @@ class SignInCard extends StatelessWidget {
             const Padding(
               padding: EdgeInsets.all(8),
               child: Text(
-                'Album Searcher for Google Photos will have read access to your photos library.',
+                'Read access to your photos library is required to enable searching.',
                 textAlign: TextAlign.center,
               ),
             ),
-            const Padding(
-              padding: EdgeInsets.all(8),
-              child: TermsOfServiceText(),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'Usage of data adheres to the ',
+                    ),
+                    TextSpan(
+                      text: 'Google API Services User Data Policy',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          await launchUrl(
+                            Uri.https(
+                              'developers.google.com',
+                              '/terms/api-services-user-data-policy',
+                            ),
+                            webOnlyWindowName: '_blank',
+                          );
+                        },
+                    ),
+                    const TextSpan(
+                      text: '.',
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8),
+              child: Text.rich(
+                TextSpan(
+                  children: [
+                    const TextSpan(
+                      text: 'By continuing you agree to the ',
+                    ),
+                    TextSpan(
+                      text: 'Privacy Policy',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          await launchUrl(
+                            Uri.https(
+                              'googlephotosalbumsearcher.thomasclark.app',
+                              '/privacy.html',
+                            ),
+                            webOnlyWindowName: '_blank',
+                          );
+                        },
+                    ),
+                    const TextSpan(
+                      text: ' and the ',
+                    ),
+                    TextSpan(
+                      text: 'Terms of Service',
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                            color: Theme.of(context).colorScheme.primary,
+                          ),
+                      recognizer: TapGestureRecognizer()
+                        ..onTap = () async {
+                          await launchUrl(
+                            Uri.https(
+                              'googlephotosalbumsearcher.thomasclark.app',
+                              '/terms.html',
+                            ),
+                            webOnlyWindowName: '_blank',
+                          );
+                        },
+                    ),
+                    const TextSpan(
+                      text: '.',
+                    ),
+                  ],
+                ),
+                textAlign: TextAlign.center,
+              ),
             ),
             Padding(
               padding: const EdgeInsets.all(8),
